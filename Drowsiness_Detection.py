6902e56a1cfaf41cac199f588d8c209ca65d330d
WALKER <divakarnaidumahanthi@gmail.com>
2024-03-06 22:42:11 +0530

    first commit

---
+++ Drowsiness_Detection.py
@@ -0,0 +1,35 @@
+import dlib
+import cv2
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+def find_area(face):
+    a = face.left()
+    b = face.top()
+    c=face.right()
+    d=face.bottom()
+    return a*b*c*d
+
+def get_coordinates(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
+
+    if len(faces)==0:
+        print("no face detected")
+    else:
+        max_area_face=faces[0]
+        max_area=0
+        for face in faces:
+            if find_area(face)>max_area:
+                max_area=find_area(face)
+                max_area_face=face
+
+        landmarks = predictor(gray, max_area_face)
+        coordinates = [[point.x, point.y] for point in landmarks.parts()]
+        return coordinates
+
+image=cv2.imread("test.jpg")
+coordinates=get_coordinates(image)
+print(coordinates) 
